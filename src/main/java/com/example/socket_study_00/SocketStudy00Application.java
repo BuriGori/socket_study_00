965b53e3657c455de873d79c144120dd3ae5ba39
bgr <jamasa97@icloud.com>
2022-03-21 22:44:16 +0900

    Initial commit

---
+++ src/main/java/com/example/socket_study_00/SocketStudy00Application.java
@@ -0,0 +1,13 @@
+package com.example.socket_study_00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketStudy00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketStudy00Application.class, args);
+    }
+
+}
